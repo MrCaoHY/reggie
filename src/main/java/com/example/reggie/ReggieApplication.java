c5aa41ec2671d03d976ed9b4d9310a46a75349fc
曹海洋 <18728191135@163.com>
2022-08-01 00:48:01 +0800

    数据库读写分离

--- src/main/java/com/example/reggie/ReggieApplication.java
+++ src/main/java/com/example/reggie/ReggieApplication.java
@@ -2,10 +2,10 @@ package com.example.reggie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
